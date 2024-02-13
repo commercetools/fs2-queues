2b95faeefd8c8d5f94b7d4bc545a053da000a847
Lucas Satabin <lucas.satabin@commercetools.com>
2024-02-13 09:16:32 +0100

    Improve behavior of the sequential subscriber
    When processing of a message fails in a batch, we can immediately nack
    all following messages in the batch, since the stream will fail. This
    reduces time when they are locked in the queue, while we know they won't
    be processed.
    
    Moreover, all previous messages that were processed in the same batch
    can be emitted, as we know they were successful.
    This second part is similar to what `evalMap` does, but the current
    approach keeps the chunk structure (`evalMap` unchunks everything),
    which can lead to better performances.

--- core/src/main/scala/de/commercetools/queue/QueueSubscriber.scala
+++ core/src/main/scala/de/commercetools/queue/QueueSubscriber.scala
@@ -1,9 +1,13 @@
 package de.commercetools.queue
 
+import cats.data.Chain
 import cats.effect.IO
-import fs2.Stream
+import fs2.{Chunk, Stream}
 
 import scala.concurrent.duration.FiniteDuration
+import fs2.Pull
+import cats.effect.kernel.Outcome
+import cats.syntax.all._
 
 /**
  * The base interface to subscribe to a queue.
@@ -35,9 +39,45 @@ trait QueueSubscriber[T] {
    * to use `attemptProcessWithAutoAck` instead.
    *
    * '''Note:''' This stream does '''not''' handle lock extension.
+   *
+   * Messages in a batch are processed sequentially, stopping at the first error.
+   * All results up to the error will be emitted downstream before failing.
    */
-  def processWithAutoAck[Res](batchSize: Int, waitingTime: FiniteDuration)(f: T => IO[Res]): Stream[IO, Res] =
-    messages(batchSize, waitingTime).evalMap(ctx => f(ctx.payload).flatTap(_ => ctx.ack()).onError(_ => ctx.nack()))
+  def processWithAutoAck[Res](batchSize: Int, waitingTime: FiniteDuration)(f: T => IO[Res]): Stream[IO, Res] = {
+    // to have full control over nacking things in time after a failure, and emitting
+    // results up to the error, we resort to a `Pull`, which allows this fine graind control
+    // over pulling/emitting/failing
+    // it keeps the chunk structure as best as it can
+    def doChunk(chunk: Chunk[MessageContext[T]], idx: Int, acc: Chain[Res]): Pull[IO, Res, Unit] =
+      if (idx >= chunk.size) {
+        // we are done, emit the chunk
+        Pull.output(Chunk.chain(acc))
+
+      } else {
+        val ctx = chunk(idx)
+        Pull
+          .eval(f(ctx.payload).guaranteeCase {
+            case Outcome.Succeeded(_) => ctx.ack()
+            case _ =>
+              // if it was cancelled or errored, let's nack this and up to the end of the chunk
+              // before failing
+              // array slice creation is O(1) as well as `drop` on an `ArraySlice`
+              chunk.toArraySlice.drop(idx).traverse_(_.nack().attempt)
+          })
+          .attempt
+          .flatMap {
+            case Right(res) => doChunk(chunk, idx + 1, acc.append(res))
+            case Left(t) =>
+              // one processing failed, emit everything that was processed in the chunk
+              // and fail
+              Pull.output(Chunk.chain(acc)).covary[IO] *> Pull.raiseError[IO](t)
+          }
+      }
+    messages(batchSize, waitingTime).repeatPull(_.uncons.flatMap {
+      case Some((hd, tl)) => doChunk(hd, 0, Chain.empty).as(Some(tl))
+      case None => Pull.pure(None)
+    })
+  }
 
   /**
    * Processes the messages with the provided processing function.
@@ -46,10 +86,13 @@ trait QueueSubscriber[T] {
    * processing errors, allowing you to build error recovery logic.
    *
    * '''Note:''' This stream does '''not''' handle lock extension.
+   *
+   * Messages in a batch are processed in parallel but result is emitted in
+   * order the messages were received.
    */
   def attemptProcessWithAutoAck[Res](batchSize: Int, waitingTime: FiniteDuration)(f: T => IO[Res])
     : Stream[IO, Either[Throwable, Res]] =
-    messages(batchSize, waitingTime).evalMapChunk(ctx =>
+    messages(batchSize, waitingTime).parEvalMap(batchSize)(ctx =>
       f(ctx.payload).attempt.flatTap {
         case Right(_) => ctx.ack()
         case Left(_) => ctx.nack()
