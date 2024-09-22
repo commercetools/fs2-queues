ce7ae7befca37feeb17913ea05f0e2cfbfe6441b
Scala Steward <scala_steward@virtuslab.com>
2024-09-22 13:40:19 +0000

    Update circe-parser to 0.14.10

--- project/Version.scala
+++ project/Version.scala
@@ -3,6 +3,6 @@ object Versions {
   val http4s = "0.23.25"
   val munit = "0.7.29"
   val munitCatsEffect = "1.0.7"
-  val circe = "0.14.7"
+  val circe = "0.14.10"
   val circeGenericExtras = "0.14.3"
 }
