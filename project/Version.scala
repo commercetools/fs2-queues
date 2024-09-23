55154bea0075b63db4e90c5822652febbc650067
Alessandro Zoffoli <alessandro.zoffoli@commercetools.com>
2024-09-23 11:50:00 +0200

    Bump munit to 1.0.2

--- project/Version.scala
+++ project/Version.scala
@@ -1,7 +1,7 @@
 object Versions {
   val fs2 = "3.11.0"
   val http4s = "0.23.25"
-  val munit = "0.7.29"
+  val munit = "1.0.2"
   val munitCatsEffect = "2.0.0"
   val circe = "0.14.10"
   val circeGenericExtras = "0.14.3"
