e2eb8b95ed72ea73267a6a87050e28bcf2f8afdf
Scala Steward <scala_steward@virtuslab.com>
2025-04-30 14:15:19 +0000

    Update munit to 1.1.1

--- project/Version.scala
+++ project/Version.scala
@@ -1,7 +1,7 @@
 object Versions {
   val fs2 = "3.11.0"
   val http4s = "0.23.25"
-  val munit = "1.0.4"
+  val munit = "1.1.1"
   val munitCatsEffect = "2.0.0"
   val circe = "0.14.10"
   val circeGenericExtras = "0.14.3"
