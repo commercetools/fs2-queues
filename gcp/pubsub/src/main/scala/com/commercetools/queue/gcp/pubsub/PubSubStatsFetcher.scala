b3f969e3519d19df28e20289b15d17bb53dec456
Alessandro Zoffoli <alessandro.zoffoli@commercetools.com>
2026-02-10 11:19:37 +0100

    Add missing error wrapping on pubsub

--- gcp/pubsub/src/main/scala/com/commercetools/queue/gcp/pubsub/PubSubStatsFetcher.scala
+++ gcp/pubsub/src/main/scala/com/commercetools/queue/gcp/pubsub/PubSubStatsFetcher.scala
@@ -18,6 +18,7 @@ package com.commercetools.queue.gcp.pubsub
 
 import cats.effect.Async
 import cats.syntax.flatMap._
+import cats.syntax.monadError._
 import com.commercetools.queue.{MalformedQueueConfigurationException, QueueStats, UnsealedQueueStatsFetcher}
 import com.google.cloud.monitoring.v3.stub.MetricServiceStub
 import com.google.monitoring.v3.{ListTimeSeriesRequest, Point, TimeInterval}
@@ -45,18 +46,18 @@ private class PubSubStatsFetcher[F[_]](
                 .setName(s"projects/${subscriptionName.getProject()}")
                 // we need to query at least one minute
                 // https://stackoverflow.com/questions/68546947/google-cloud-metrics-get-pub-sub-metric-filtered-by-subscription-id
-                .setInterval(
-                  TimeInterval
-                    .newBuilder()
-                    .setStartTime(Timestamp.newBuilder().setSeconds(now.toSeconds - 61).build())
-                    .setEndTime(Timestamp.newBuilder().setSeconds(now.toSeconds).build())
-                    .build())
+                .setInterval(TimeInterval
+                  .newBuilder()
+                  .setStartTime(Timestamp.newBuilder().setSeconds(now.toSeconds - 61).build())
+                  .setEndTime(Timestamp.newBuilder().setSeconds(now.toSeconds).build())
+                  .build())
                 .setFilter(
                   s"""metric.type="pubsub.googleapis.com/subscription/num_undelivered_messages" AND resource.label.subscription_id = "${subscriptionName
                       .getSubscription()}"""")
                 .build())
 
         })
+          .adaptError(makeQueueException(_, queueName))
       }
       .flatMap { response =>
         if (response.getTimeSeriesCount == 0) F.pure(QueueStats(0, None, None))
